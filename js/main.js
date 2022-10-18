413469eb23bd65d3aec7680c8202a727c036f475
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-10-18 10:10:03 -0700

    test fetch api

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
+//XMLHttpRequest
 const request = new XMLHttpRequest();
 request.open("GET","http://localhost:8080/api/v1/user/getUser");
 request.send();
 request.onload=()=>{
   console.log(JSON.parse(request.response))
 }
+
+//fetch api
+
+fetch("http://localhost:8080/api/v1/user/getUser")
+.then(response=>{
+  return response.json();
+}).then(json=>{
+  console.log(json)
+})
