54740dff0b653066b7c2e6fceb965c4a1b09164c
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-10-18 10:00:13 -0700

    XMLHttpRequest test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+const request = new XMLHttpRequest();
+request.open("GET","http://localhost:8080/api/v1/user/getUser");
+request.send();
+request.onload=()=>{
+  console.log(JSON.parse(request.response))
+}
